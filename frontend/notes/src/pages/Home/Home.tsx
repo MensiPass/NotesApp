4f6596fe4e972257c2781936e325707ad8ae8570
Mensura <mensipass@gmail.com>
2024-07-11 14:19:28 +0200

    search bar added

--- frontend/notes/src/pages/Home/Home.tsx
+++ frontend/notes/src/pages/Home/Home.tsx
@@ -1,10 +1,14 @@
 import React from "react";
 import Navbar from "../../components/Navbar/Navbar";
+import NoteCard from "../../components/Cards/NoteCard";
 
 const Home = () => {
   return (
     <>
       <Navbar />
+      <div className="container mx-auto">
+        <NoteCard />
+      </div>
     </>
   );
 };
